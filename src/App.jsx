1a63e380203b73a6850860abd3187feb78830516
achinthaprabash3 <achinthaprabash3@gmail.com>
2025-04-23 19:28:02 +0530

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "motion/react";
-import HeroSection from "../custom/heroSection";
+
 import FeaturesSection from "../custom/FeaturesSection";
 import ProgramSection from "../custom/ProgramSection";
 import ReviewSection from "../custom/ReviewSection";
@@ -9,11 +9,11 @@ import QandASection from "../custom/QandASection";
 import ContactSection from "../custom/ContactSection";
 import FooterSection from "../custom/FooterSection";
 import PageWithPopup from "../custom/PageWithPupup";
+import HeroSection from "../custom/HeroSection";
 
 function App() {
   return (
     <>
-      {/* <PageWithPopup /> */}
       <HeroSection />
       <FeaturesSection />
       <ProgramSection />
